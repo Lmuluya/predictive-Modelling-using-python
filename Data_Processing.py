5ef5c45970ee50e20e1cce08a4f3feae3b7590e0
Lamech Muluya Kibudde <36754197+Lmuluya@users.noreply.github.com>
2020-10-04 18:53:02 +0300

    Add files via upload

--- Data_Processing.py
+++ Data_Processing.py
@@ -4,6 +4,10 @@ import pandas as pd
 
 import os
 
+from sklearn.impute import SimpleImputer
+
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder
+
 os.getcwd()
 
 ## importing dataset 
@@ -24,3 +28,25 @@ data = pd.read_csv("Data.csv")
 x = data.iloc[:,:-1].values
 
 y = data.iloc[:,3].values
+
+# Data modeling
+simp = SimpleImputer(missing_values = 'NaN', strategy = 'mean')
+simp = SimpleImputer().fit(x[:, 1:3]) 
+x[:, 1:3] = simp.transform(x[:, 1:3])
+
+# Encoding the cartegorical variables
+label_encoder_y = LabelEncoder()
+
+y = label_encoder_y.fit_transform(y)
+
+# lable encoding based on countries
+label_encoder_x = LabelEncoder()
+
+
+x[:,0] = label_encoder_x.fit_transform(x[:,0])
+
+# Creating dummy variables
+onehot = OneHotEncoder(categorical_features = [0])
+x = onehot.fit_transform(x).toarray()
+
+
