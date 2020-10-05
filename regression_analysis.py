d593d91579b585d14a51653199e5151f09ada98d
Lamech Muluya Kibudde <36754197+Lmuluya@users.noreply.github.com>
2020-10-05 21:21:21 +0300

    Add files via upload

--- regression_analysis.py
+++ regression_analysis.py
@@ -1,7 +1,13 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Oct  4 22:33:19 2020
+# Importing the relevant libraries 
 
-@author: LMULUYA
-"""
+import pandas as pd
+
+import numpy as np
+
+dataset = pd.read_csv("C:\\Users\\mxp comps\\Desktop\\Educba\\Machine Learning Projects\\Projects on ML - Predictive Modeling with Python\\Linear Regression\\tip.csv")
+
+# Defining the variables
+x = dataset.iloc[:, :-1].values
+
+y = dataset.iloc[:, 1].values 
 
