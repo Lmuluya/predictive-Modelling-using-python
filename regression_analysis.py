50273a3aba4923c81235f239322c008ef9138c71
Lamech Muluya Kibudde <36754197+Lmuluya@users.noreply.github.com>
2020-10-08 17:14:16 +0300

    Add files via upload

--- regression_analysis.py
+++ regression_analysis.py
@@ -1,8 +1,11 @@
 # Importing the relevant libraries 
 
 import pandas as pd
-
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+import statsmodels.formula.api as sf
+
 
 dataset = pd.read_csv("C:\\Users\\mxp comps\\Desktop\\Educba\\Machine Learning Projects\\Projects on ML - Predictive Modeling with Python\\Linear Regression\\tip.csv")
 
@@ -11,3 +14,34 @@ x = dataset.iloc[:, :-1].values
 
 y = dataset.iloc[:, 1].values 
 
+# Fitting a reegression line
+regressor = LinearRegression()
+
+regressor.fit(x,y)
+
+# Getting the coefficients 
+regressor.coef_
+
+regressor.intercept_
+
+# Plotting the variables
+x_mean = [np.mean(x) for i in x]
+
+y_mean = [np.mean(y) for i in y]
+
+plt.scatter(x,y)
+plt.plot(x, regressor.predict(x), color = "red")
+plt.plot(x, y_mean, linestyle = "--")
+plt.plot(x_mean, y, linestyle = "--")
+plt.title("Tip VS Bill")
+plt.xlabel("Bill in $")
+plt.ylabel("Tip in $")
+plt.show()
+
+# Summarised model
+
+lm = sf.ols(formula = "Tip ~ Bill", data = dataset).fit()
+
+lm.params
+
+print(lm.summary())
