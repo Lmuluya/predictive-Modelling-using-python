265bd349310a90fb2fa2ea86704683380984f895
Lamech Muluya Kibudde <36754197+Lmuluya@users.noreply.github.com>
2020-10-08 17:33:33 +0300

    Add files via upload

--- salary_prediction.py
+++ salary_prediction.py
@@ -1,7 +1,12 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Oct  8 17:10:59 2020
+# importing the required libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-@author: LMULUYA
-"""
+# Import the data set
+dataset = pd.read_csv("C:\\Users\\mxp comps\\Desktop\\Educba\\Machine Learning Projects\\Projects on ML - Predictive Modeling with Python\\Linear Regression\\Salary_Data.csv")
 
+# Selecting the data 
+x = dataset.iloc[:, :-1].values
+
+y = dataset.iloc[:, 1].values
