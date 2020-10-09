da50794b8e4089c0ec4af26012a6e6c9c76769a8
Lamech Muluya Kibudde <36754197+Lmuluya@users.noreply.github.com>
2020-10-09 15:35:57 +0300

    Add files via upload

--- salary_prediction.py
+++ salary_prediction.py
@@ -10,3 +10,16 @@ dataset = pd.read_csv("C:\\Users\\mxp comps\\Desktop\\Educba\\Machine Learning P
 x = dataset.iloc[:, :-1].values
 
 y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into test and train dataset 
+
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(x,y,
+                                                    test_size = 1/3, random_state = 0)
+
+# fitting a simple linear model to training set
+from sklearn.linear_model import LinearRegression
+
+
+
